c3029da09173230898bfd030dacbce51a13fe0ad
Zero-Ya <ketyanuk111@gmail.com>
2026-06-10 15:29:20 +0700

    Added application class

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -1,2 +1,70 @@
 #include "application.h"
 
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <iostream>
+
+// Executes whenever window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+Application::Application(const char* title, int width, int height)
+    : m_Width(width), m_Height(height), m_Window(nullptr)
+{
+    initWindow(title);
+}
+
+Application::~Application()
+{
+    glfwTerminate();
+}
+
+bool Application::isRunning() const
+{
+    return !glfwWindowShouldClose(m_Window);
+}
+
+void Application::clearScreen(float r, float g, float b, float a) const
+{
+    glClearColor(r, g, b, a);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Application::swapBuffers() const
+{
+    glfwSwapBuffers(m_Window);
+}
+
+void Application::initWindow(const char* title)
+{
+    // GLFW: initialize and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // GLFW window creation
+    m_Window = glfwCreateWindow(m_Width, m_Height, title, nullptr, nullptr);
+    if (m_Window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(m_Window);
+    glfwSetFramebufferSizeCallback(m_Window, framebuffer_size_callback);
+
+    // GLAD: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
--- src/core/application.h
+++ src/core/application.h
@@ -1,2 +1,28 @@
 #pragma once
 
+struct GLFWwindow;
+
+class Application {
+public:
+	Application(const char* title, int width, int height);
+	~Application();
+
+	// Controls
+	bool isRunning() const;
+	void clearScreen(float r, float g, float b, float a) const;
+	void swapBuffers() const;
+
+	// Window getter
+	GLFWwindow* getWindow() const { return m_Window; }
+
+	// Dimensions getter
+	int getWidth() const { return m_Width; }
+	int getHeight() const { return m_Height; }
+
+private:
+	void initWindow(const char* title);
+
+	GLFWwindow* m_Window;
+	int m_Width;
+	int m_Height;
+};
