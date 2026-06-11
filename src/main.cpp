3b77078c836ba84d62368b4977ac2dde6e17bd01
Zero-Ya <ketyanuk111@gmail.com>
2026-06-11 20:40:46 +0700

    Added shader class, shader files and made an ImGui demo

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,21 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <imgui.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+
 #include <iostream>
 
 // Core includes
 #include "core/application.h"
+
+// Rendering
+#include "rendering/utils/shader.h"
 
 void processInput(GLFWwindow* window);
 
@@ -15,7 +26,49 @@ const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
+    // Init app
     Application app("Physics Engine", SCR_WIDTH, SCR_HEIGHT);
+
+    // Shaders and buffers
+    Shader basicShader("assets/shaders/2d_basic_shader.vs", "assets/shaders/2d_basic_shader.fs");
+
+    float vertices[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // ImGui init
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(app.getWindow(), true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // Uniform data
+    float size = 1.0f;
+    glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
+    basicShader.use();
+    basicShader.setFloat("size", size);
+    basicShader.setVec4("color", color);
 
     // Render loop
     while (app.isRunning())
@@ -26,10 +79,39 @@ int main()
         // Render
         app.clearScreen(0.2f, 0.3f, 0.3f, 1.0f);
 
+        // Render the triangle
+        basicShader.use();
+        basicShader.setFloat("size", size);
+        basicShader.setVec4("color", color);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Window");
+        ImGui::Text("Hello world!");
+        ImGui::SliderFloat("Size", &size, 0.5f, 2.0f);
+        ImGui::ColorEdit4("Color", &color.r);
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         // GLFW: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         app.swapBuffers();
         glfwPollEvents();
     }
+
+    // Optional
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     return 0;
 }
